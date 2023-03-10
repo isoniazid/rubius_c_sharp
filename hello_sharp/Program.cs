0cf8f126d301d2aa50390b5803a503387b6865da
Vasya Ryzhkoff <ryzh.v.n@mail.ru>
2023-03-10 17:25:18 +0700

    Первая домашка

---
+++ hello_sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+
+    private static void setName(string name = "")
+    {
+        _name_ = name;
+    }
+
+    private static string getName()
+    {
+        return _name_;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Как тебя зовут?");
+        setName(Console.ReadLine());
+        Console.WriteLine($"{getName()}, привет!");
+    }
+
+
+    private static string _name_ = "";
+}
+
